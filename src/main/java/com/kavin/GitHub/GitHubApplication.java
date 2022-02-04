0a4f7571ac8480583d2b3a91be5bd37d9fd9c93a
Kavinkumar01 <kavinkumardev7@gmail.com>
2022-02-04 17:24:15 +0530

    added new class for dynamic message

--- src/main/java/com/kavin/GitHub/GitHubApplication.java
+++ src/main/java/com/kavin/GitHub/GitHubApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitHubApplication {
-
+	
+	Message message= new Message();
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubApplication.class, args);
 	}
 	
 	public void print(){
-		System.out.println("Hello World");
+		System.out.println(message.getMessage());
 	}
 
 }
