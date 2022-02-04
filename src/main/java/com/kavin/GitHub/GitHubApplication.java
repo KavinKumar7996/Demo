e166ead58cd49c45951422d7ffb268fb117cb75f
Kavinkumar01 <95081636+Kavinkumar01@users.noreply.github.com>
2022-02-04 17:16:17 +0530

    Update GitHubApplication.java

--- src/main/java/com/kavin/GitHub/GitHubApplication.java
+++ src/main/java/com/kavin/GitHub/GitHubApplication.java
@@ -9,5 +9,9 @@ public class GitHubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubApplication.class, args);
 	}
+	
+	public void print(){
+		System.out.println("Hello World");
+	}
 
 }
