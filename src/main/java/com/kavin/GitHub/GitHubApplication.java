8cbc376dac88fa524d1aad80d479ab011f5b52bf
Kavinkumar01 <kavinkumardev7@gmail.com>
2022-02-05 19:39:38 +0530

    added logger

--- src/main/java/com/kavin/GitHub/GitHubApplication.java
+++ src/main/java/com/kavin/GitHub/GitHubApplication.java
@@ -1,5 +1,7 @@
 package com.kavin.GitHub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,13 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitHubApplication {
 	
 	Message message= new Message();
-	
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubApplication.class, args);
 	}
+	Logger logg=LoggerFactory.getLogger(GitHubApplication.class);
 	
 	public void print(){
+		logg.debug(message.getMessage());
 		System.out.println(message.getMessage());
 	}
+	
 
 }
